6f6c836dc54534628e74881f07120cd135b474fb
Argentum <argentum@maximail.org>
2015-10-29 18:30:59 +0500

    OwlRequester умеет работать с api и с mysql

--- src/Library/OwlRequester.php
+++ src/Library/OwlRequester.php
@@ -18,19 +18,32 @@ class OwlRequester
 
     public function request($url, $useOwlServer = true)
     {
-        $url = $this->getUrl($url);
-        $request = new Request($url);
-        $request->getOptions()
-            ->set(CURLOPT_TIMEOUT, 8)
-            ->set(CURLOPT_RETURNTRANSFER, true)
-            ->set(CURLOPT_USERAGENT, $this->di->get('config')->curlUserAgent);
-
-        $response = $request->send();
-        $response = json_decode($response->getContent(), true);
-
         PH_DEBUG ? Debugger::dumpBar($url, 'url') : null;
 
-        return $response;
+        if ($useOwlServer) {
+
+            $url = $this->getUrl($url);
+            $request = new Request($url);
+            $request->getOptions()
+                ->set(CURLOPT_TIMEOUT, 8)
+                ->set(CURLOPT_RETURNTRANSFER, true)
+                ->set(CURLOPT_USERAGENT, $this->di->get('config')->curlUserAgent);
+
+            $response = $request->send();
+            $response = json_decode($response->getContent(), true);
+
+            return $response;
+        }
+
+        /** @var Urls $urls */
+        $urls = Urls::findFirst([
+                'url = :url:',
+                'bind' => [
+                    'url' => $url
+                ]
+            ]);
+
+        return json_decode($urls->content, true);
     }
 
     public function getUrl($rawUrl)
