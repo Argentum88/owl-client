24613479e36aa5f8052c409df93981416d5553d3
Argentum <argentum@maximail.org>
2015-10-29 15:47:35 +0500

    унификация

--- src/Library/OwlRequester.php
+++ src/Library/OwlRequester.php
@@ -2,37 +2,49 @@
 
 namespace Client\Library;
 
-use Core\Debugger;
+use Client\Library\Debugger;
+use cURL\Request;
 use Phalcon\Debug;
 use Phalcon\Di;
 
-class OwlRequester extends CurlRequester
+class OwlRequester
 {
     protected $di;
 
     public function __construct()
     {
         $this->di = Di::getDefault();
-        parent::__construct();
     }
 
-    public function request($url, $curlOpts = [])
+    public function request($url, $useOwlServer = true)
+    {
+        $url = $this->getUrl($url);
+        $request = new Request($url);
+        $request->getOptions()
+            ->set(CURLOPT_TIMEOUT, 8)
+            ->set(CURLOPT_RETURNTRANSFER, true)
+            ->set(CURLOPT_USERAGENT, $this->di->get('config')->curlUserAgent);
+
+        $response = $request->send();
+        $response = json_decode($response->getContent(), true);
+
+        PH_DEBUG ? Debugger::dumpBar($url, 'url') : null;
+
+        return $response;
+    }
+
+    public function getUrl($rawUrl)
     {
         $clientId = $this->di->get('config')->clientId;
         $secretKey = $this->di->get('config')->secretKey;
-        $string = "client_id=$clientId&url=$url$secretKey";
+        $string = "client_id=$clientId&url=$rawUrl$secretKey";
 
         $params = http_build_query([
-            'client_id' => $clientId,
-            'url' => $url,
-            'sig' => md5($string)
-        ]);
+                'client_id' => $clientId,
+                'url' => $rawUrl,
+                'sig' => md5($string)
+            ]);
 
-        $url = $this->di->get('config')->owl . "/api/1.0/?$params";
-        $response = parent::request($url, $curlOpts);
-
-        PH_DEBUG ? Debugger::dumpBar($url,'url') : null;
-
-        return $response;
+        return $this->di->get('config')->owl . "/api/1.0/?$params";
     }
 }
