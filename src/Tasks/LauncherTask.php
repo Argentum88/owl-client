4f74012fac9aaa7ae334a99ed6f6e5b3bd1d9b4a
Argentum <argentum@maximail.org>
2015-12-17 16:55:09 +0500

    забабахали баннеры к быстрой системе

--- src/Tasks/LauncherTask.php
+++ src/Tasks/LauncherTask.php
@@ -2,6 +2,7 @@
 
 namespace Client\Tasks;
 
+use Client\Models\UpdateBannerLaunchers;
 use Client\Models\UpdateContentLaunchers;
 use Phalcon\CLI\Task;
 
@@ -10,6 +11,12 @@ class LauncherTask extends Task
     public function launchUpdateContentAction()
     {
         $launcher = new UpdateContentLaunchers();
+        $launcher->start();
+    }
+
+    public function launchUpdateBannerAction()
+    {
+        $launcher = new UpdateBannerLaunchers();
         $launcher->start();
     }
 }
